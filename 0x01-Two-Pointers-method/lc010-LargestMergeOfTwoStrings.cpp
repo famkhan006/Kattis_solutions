14bbfb9e3e61d9d6d60804850caafce711d92996
poricf <famkhan006@gmail.com>
2023-11-26 23:37:17 +0300

    LargestMerge

--- 0x01-Two-Pointers-method/lc010-LargestMergeOfTwoStrings.cpp
+++ 0x01-Two-Pointers-method/lc010-LargestMergeOfTwoStrings.cpp
@@ -7,17 +7,14 @@ public:
         int i = 0, j = 0;
         
         while (i < word1.size() && j < word2.size()) {
-            if (word1[i] > word2[j]) {
+            if (word1[i] > word2[j])       
                 merge += word1[i++];
-            } else if (word1[i] < word2[j]) {
-                merge += word2[j++];
-            } else {
-                if (word1.substr(i) >= word2.substr(j)) {
-                    merge += word1[i++];
-                } else {
-                    merge += word2[j++];
-                }
-            }
+                
+            else if (word1[i] == word2[j])  
+            (word1.substr(i) >= word2.substr(j))? merge += word1[i++]
+                                                : merge += word2[j++];
+            else 
+                merge += word2[j++];                            
         }
         
         merge += word1.substr(i);
