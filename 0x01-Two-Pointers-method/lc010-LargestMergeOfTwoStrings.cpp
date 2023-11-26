938e03b43a56228afdaf65e928e944821ab7e110
poricf <famkhan006@gmail.com>
2023-11-26 23:42:24 +0300

    LargestMerge

--- 0x01-Two-Pointers-method/lc010-LargestMergeOfTwoStrings.cpp
+++ 0x01-Two-Pointers-method/lc010-LargestMergeOfTwoStrings.cpp
@@ -1,4 +1,7 @@
-#include <string>
+/* Author - Fahmi-Dinsefa
+   credit - Nasiha Abdella and chatGpt
+
+*/
 
 class Solution {
 public:
@@ -7,12 +10,8 @@ public:
         int i = 0, j = 0;
         
         while (i < word1.size() && j < word2.size()) {
-            if (word1[i] > word2[j])       
+            if (word1.substr(i) > word2.substr(j))       
                 merge += word1[i++];
-                
-            else if (word1[i] == word2[j])  
-            (word1.substr(i) >= word2.substr(j))? merge += word1[i++]
-                                                : merge += word2[j++];
             else 
                 merge += word2[j++];                            
         }
